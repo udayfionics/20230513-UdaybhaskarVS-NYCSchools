854c5de93e091ddd39b14f3b0101fb52bc8ea372
udayfionics <udayvemps@gmail.com>
2023-05-13 23:48:27 +0530

    Fragment navigate up added

--- app/src/main/java/udayfionics/nycschools/view/MainActivity.kt
+++ app/src/main/java/udayfionics/nycschools/view/MainActivity.kt
@@ -1,12 +1,24 @@
 package udayfionics.nycschools.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import udayfionics.nycschools.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)!!
+            .findNavController()
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
