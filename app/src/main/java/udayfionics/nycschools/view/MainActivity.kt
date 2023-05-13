f76531c668f2a7405c879372358c1e1a17310004
udayfionics <udayvemps@gmail.com>
2023-05-13 22:35:23 +0530

    MainActivity.kt moved to view package

--- app/src/main/java/udayfionics/nycschools/view/MainActivity.kt
+++ app/src/main/java/udayfionics/nycschools/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package udayfionics.nycschools
+package udayfionics.nycschools.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import udayfionics.nycschools.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
