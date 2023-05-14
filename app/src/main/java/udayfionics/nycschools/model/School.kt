ea207890e5670ee0c1156d2129834bd75224993c
udayfionics <udayvemps@gmail.com>
2023-05-15 00:24:58 +0530

    School.kt Model class Created

---
+++ app/src/main/java/udayfionics/nycschools/model/School.kt
@@ -0,0 +1,24 @@
+package udayfionics.nycschools.model
+
+data class School(
+    val dbn: String? = null,
+    val schoolName: String? = null,
+    val totalStudents: String? = null,
+    val languageClasses: String? = null,
+    val startTime: String? = null,
+    val endTime: String? = null,
+    val overviewParagraph: String? = null,
+    val attendanceRate: String? = null,
+    val graduationRate: String? = null,
+    val phoneNumber: String? = null,
+    val faxNumber: String? = null,
+    val schoolEmail: String? = null,
+    val website: String? = null,
+    val primaryAddressLine1: String? = null,
+    val city: String? = null,
+    val stateCode: String? = null,
+    val zip: String? = null,
+    val latitude: String? = null,
+    val longitude: String? = null,
+)
+
